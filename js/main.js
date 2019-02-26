f5e1205686574fc406559ff0ec678a23ecda247f
Test <bsmith20681@gmail.com>
2019-02-25 19:59:38 -0700

    added input field for flashcard

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+var input1 = document.getElementById('form1');
+var input2 = document.getElementById('form2');
+var textOnCard = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+  document.getElementById('flashcard__front').innerHTML = input1.value
+  document.getElementById('flashcard__back').innerHTML = input2.value
+})
+
+
+
 var flashcard = document.querySelector('.flashcard');
 
 flashcard.addEventListener('click', function() {
