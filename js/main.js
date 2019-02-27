5a1862105f486a998421b666db6a28caf5462cdb
Test <bsmith20681@gmail.com>
2019-02-26 18:18:40 -0700

    added function to create new input forms on button click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//TAKES INPUT FORM AND PLACES TEXT ON CARD
 var input1 = document.getElementById('form1');
 var input2 = document.getElementById('form2');
 var textOnCard = document.getElementById('btn');
@@ -8,9 +9,20 @@ btn.addEventListener('click', function() {
 })
 
 
-
+//FUNCTION THAT ALLOWS CARD TO ROTATE
 var flashcard = document.querySelector('.flashcard');
 
 flashcard.addEventListener('click', function() {
   flashcard.classList.toggle('flip');
+});
+
+
+
+//CREATES A NEW INPUT FIELD
+var inputHtml = '<div class="container mt-5"><div class="row"><div class="col-lg-12 text-center"><form id="form" action=""><div><input type="text" id="form1">'+ ' ' + '<input type="text" id="form2">' + ' ' + '<button class="btn btn-primary" id="btn">SUBMIT</button></div></form></div></div></div>'
+
+var addInputField = document.getElementById('addInputField');
+
+addInputField.addEventListener('click', function() {
+  textOnCard.insertAdjacentHTML("afterend", inputHtml)
 });
