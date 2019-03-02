158c50ccaee342f71908ebbbf5f009debbe66474
Test <bsmith20681@gmail.com>
2019-03-01 18:42:45 -0700

    added prototype method which allows to selector all of the input fields

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 //TAKES INPUT FORM AND PLACES TEXT ON CARD
-var input1 = document.getElementById('form1');
-var input2 = document.getElementById('form2');
+var input1 = document.querySelectorAll('.form1');
+var input2 = document.querySelectorAll('.form2');
 var textOnCard = document.getElementById('btn');
 
-btn.addEventListener('click', function() {
+/*btn.addEventListener('click', function() {
   document.getElementById('flashcard__front').innerHTML = input1.value
   document.getElementById('flashcard__back').innerHTML = input2.value
-})
+});*/
 
 
 //FUNCTION THAT ALLOWS CARD TO ROTATE
@@ -17,37 +17,28 @@ flashcard.addEventListener('click', function() {
 });
 
 
-
-//CREATES A NEW INPUT FIELD
-var inputHtml = '<div class="container mt-5"><div class="row"><div class="col-lg-12 text-center"><form id="form" action=""><div><input type="text" id="form1">'+ ' ' + '<input type="text" id="form2">' + ' ' + '<button class="btn btn-primary" id="btn">SUBMIT</button></div></form></div></div></div>'
-
-var addInputField = document.getElementById('addInputField');
-
-addInputField.addEventListener('click', function() {
-  textOnCard.insertAdjacentHTML("afterend", inputHtml)
-});
-
-
 //make array apppear on screen
 
-var flashcardFront = [];
-var flashcardBack = [];
 
 var number = 0;
 
 var rightArrow = document.getElementById('rightArrow');
+var leftArrow = document.getElementById('leftArrow');
 
-var testField = document.getElementById('testField')
-
-btn.addEventListener('click', function(){
-  flashcardFront.push(input1.value);
-  flashcardBack.push(input2.value);
-
-  flashcard__front.innerHTML = flashcardFront[0];
-  flashcard__back.innerHTML = flashcardBack[0];
+var flashcardFront = Array.prototype.slice.call(input1);
+var flashcardBack = Array.prototype.slice.call(input2);
 
 
-})
+textOnCard.addEventListener('click', function(){
+
+  flashcard__front.innerHTML = flashcardFront[number].value;
+  flashcard__back.innerHTML = flashcardBack[number].value;
+
+  console.log(flashcardFront);
+});
+
+
+
 
 rightArrow.addEventListener('click', function(){
   if (number < flashcardFront.length - 1 && number < flashcardBack.length - 1){
@@ -56,15 +47,12 @@ rightArrow.addEventListener('click', function(){
     number = 0
   }
   console.log(number);
-  document.getElementById('flashcard__front').innerHTML = flashcardFront[number];
-  document.getElementById('flashcard__back').innerHTML = flashcardBack[number];
+  document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
+  document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
 
 });
 
 
-
-
-var leftArrow = document.getElementById('leftArrow');
 
 leftArrow.addEventListener('click', function(){
   if (number > flashcardFront.length && number > flashcardBack.length){
@@ -73,11 +61,16 @@ leftArrow.addEventListener('click', function(){
     number = 0
   }
   console.log(number);
-  document.getElementById('flashcard__front').innerHTML = flashcardFront[number];
-  document.getElementById('flashcard__back').innerHTML = flashcardBack[number];
+  document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
+  document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
 
 });
 
+//CREATES A NEW INPUT FIELD
+var inputHtml = '<div class="container mt-5"><div class="row"><div class="col-lg-12 text-center"><div><input type="text" class="form1">'+ ' ' + '<input type="text" class="form2">' + ' ' + '</div></div></div>'
 
+var addInputField = document.getElementById('addInputField');
 
-//document.getElementById('testField').innerHTML = testArray[number];
+addInputField.addEventListener('click', function() {
+  textOnCard.insertAdjacentHTML("afterend",inputHtml)
+});
