26ee8be46c9ac2dfd17ffd78b076b6f39d11af1a
Test <bsmith20681@gmail.com>
2019-03-04 20:47:35 -0700

    nested left and right arrow functions in order to allow the index to be changed with the new array

--- js/main.js
+++ js/main.js
@@ -11,65 +11,40 @@ flashcard.addEventListener('click', function() {
 });
 
 
-//make array apppear on screen
+//TAKE DATA FROM INPUT FIELDS, CONVERT FROM NODE LIST TO ARRAY, HAVE THEM DISPLAYED ON SCREEN ONE AT A TIME
 
 var number = 0;
 
 var rightArrow = document.getElementById('rightArrow');
 var leftArrow = document.getElementById('leftArrow');
 
-var flashcardFront = Array.prototype.slice.call(input1);
-var flashcardBack = Array.prototype.slice.call(input2);
 
 
-textOnCard.addEventListener('click', function(){
-
-  flashcard__front.innerHTML = flashcardFront[number].value;
-  flashcard__back.innerHTML = flashcardBack[number].value;
-
-});
-
-$(document).ready(function() {
-  $(textOnCard).on('click', input1, function(){
+$(textOnCard).on('click', function(){
     var input1 = document.querySelectorAll('.form1');
-    var flashcardFront = Array.prototype.slice.call(input1);
-    console.log(flashcardFront)
-  });
+    var input2 = document.querySelectorAll('.form2');
+    var flashcardFront = Array.from(input1);
+    var flashcardBack = Array.from(input2);
+    document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
+    document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
+      rightArrow.addEventListener('click', function(){
+        number++
+        document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
+        document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
+        console.log(number)
+      });
+      leftArrow.addEventListener('click', function(){
+        number--
+        document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
+        document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
+        console.log(number)
+      });
 });
-
-
-
-
-rightArrow.addEventListener('click', function(){
-  if (number < flashcardFront.length - 1 && number < flashcardBack.length - 1){
-    number++;
-  } else {
-    number = 0
-  }
-
-  document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
-  document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
-
-});
-
-leftArrow.addEventListener('click', function(){
-  if (number > flashcardFront.length && number > flashcardBack.length){
-    number--;
-  } else {
-    number = 0
-  }
-
-  document.getElementById('flashcard__front').innerHTML = flashcardFront[number].value;
-  document.getElementById('flashcard__back').innerHTML = flashcardBack[number].value;
-
-});
-
-
-
 
 
 //CREATES A NEW INPUT FIELD
 var inputHtml = '<div class="container mt-5"><div class="row"><div class="col-lg-12 text-center"><div><input type="text" class="form1">'+ ' ' + '<input type="text" class="form2">' + ' ' + '</div></div></div>'
+
 
 var addInputField = document.getElementById('addInputField');
 
